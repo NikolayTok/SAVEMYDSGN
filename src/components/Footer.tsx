3b23d0377592a78a4f748e1c0fce6388fe6bd7df
tokkaya79 <tokkaya79@gmail.com>
2024-05-14 17:50:27 +0200

    created section Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,73 @@
+import Image from "../../node_modules/next/image";
+import Link from "../../node_modules/next/link";
+
+import "../styles/footer.scss";
+
+const Footer = () => {
+	return (
+		<footer className='footer'>
+			<div className='footer__wrapper'>
+				<div className='footer__wrapper-left'>
+					<Image
+						src='/images/icons/logo-snowflake.svg'
+						alt='logo snowflake'
+						width={36}
+						height={36}
+						className='footer__logo'
+					/>
+					<ul className='footer__list'>
+						<li className='footer__list-item'>
+							<Link href='/'>Terms of Service</Link>
+						</li>
+						<li className='footer__list-item'>
+							<Link href='/'>Privacy policy</Link>
+						</li>
+						<li className='footer__list-item'>
+							<Link href='/'>Offer terms</Link>
+						</li>
+						<li className='footer__list-item'>
+							<Link href='/'>Resources</Link>
+						</li>
+					</ul>
+				</div>
+				<ul className='footer__social'>
+					<li className='footer__social-item'>
+						<Link href='/'>
+							<Image
+								src='/images/icons/twitter.svg'
+								alt='logo snowflake'
+								width={21.6}
+								height={21.6}
+								className='footer__icon'
+							/>
+						</Link>
+					</li>
+					<li className='footer__social-item'>
+						<Link href='/'>
+							<Image
+								src='/images/icons/instagram.svg'
+								alt='logo snowflake'
+								width={21.6}
+								height={21.6}
+								className='footer__icon'
+							/>
+						</Link>
+					</li>
+					<li className='footer__social-item'>
+						<Link href='/'>
+							<Image
+								src='/images/icons/facebook.svg'
+								alt='logo snowflake'
+								width={21.6}
+								height={21.6}
+								className='footer__icon'
+							/>
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import Header from './Header';
-import '../styles/globals.scss';
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import "../styles/globals.scss";
 
 const Layout = ({ children }: any) => {
-  return (
-    <div>
-      <Header />
-      <main>{children}</main>
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<main>{children}</main>
+			<Footer />
+		</div>
+	);
 };
 
 export default Layout;
