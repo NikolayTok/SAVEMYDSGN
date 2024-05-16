d66ea4406074e0d033690a0fef1d89a0ef9844d3
tokkaya79 <tokkaya79@gmail.com>
2024-05-16 17:12:17 +0200

    fixed errors

--- src/components/Creation.tsx
+++ src/components/Creation.tsx
@@ -1,48 +1,50 @@
 import CreationItems from "./CreationItems";
-
 import "../styles/creation.scss";
 
 const Creation = () => {
-  const creationItems = [
-    {
-      image: '/images/gallery/background-graphic.png',
-      progect: 'Dressx.',
-      discr: 'Rethinking UI for Digital Metaverse',
-    },
-    {
-      image: '/images/gallery/background-graphic.png',
-      progect: 'Dressx.',
-      discr: 'Rethinking UI for Digital Metaverse'
-    },
-    {
-      image: '/images/gallery/background-graphic.png',
-      progect: 'Dressx.',
-      discr: 'Rethinking UI for Digital Metaverse'
-    },
-    {
-      image: '/images/gallery/background-graphic.png',
-      progect: 'Dressx.',
-      discr: 'Rethinking UI for Digital Metaverse'
-    },
-    {
-      image: '/images/gallery/background-graphic.png',
-      progect: 'Dressx.',
-      discr: 'Rethinking UI for Digital Metaverse'
-    }
-  ]
+	const creationItems = [
+		{
+			image: "/images/background-graphic.png",
+			project: "Dressx.",
+			description: "Rethinking UI for Digital Metaverse",
+		},
+		{
+			image: "/images/background-graphic.png",
+			project: "Dressx.",
+			description: "Rethinking UI for Digital Metaverse",
+		},
+		{
+			image: "/images/background-graphic.png",
+			project: "Dressx.",
+			description: "Rethinking UI for Digital Metaverse",
+		},
+		{
+			image: "/images/background-graphic.png",
+			project: "Dressx.",
+			description: "Rethinking UI for Digital Metaverse",
+		},
+		{
+			image: "/images/background-graphic.png",
+			project: "Dressx.",
+			description: "Rethinking UI for Digital Metaverse",
+		},
+	];
 
-  return (
-    <section className="creation">
-      <div className="container">
-        <div className="creation__block">
-          <h2 className="creation__title-black">From Concept to Creation:<br></br><span className="creation__title-grey">
-            full-Spectrum Creativity in One Place.</span>
-          </h2>
-          <CreationItems items={creationItems} />
-        </div>
-      </div>
-    </section>
-  )
-}
+	return (
+		<section className='creation'>
+			<div className='container'>
+				<div className='creation__block'>
+					<h2 className='creation__title-black'>
+						From Concept to Creation:<br></br>
+						<span className='creation__title-grey'>
+							full-Spectrum Creativity in One Place.
+						</span>
+					</h2>
+					<CreationItems items={creationItems} />
+				</div>
+			</div>
+		</section>
+	);
+};
 
-export default Creation
+export default Creation;
--- src/components/CreationItems.tsx
+++ src/components/CreationItems.tsx
@@ -1,26 +1,25 @@
 import CreationItem from "./CreationItem";
-
 import "../styles/creationItems.scss";
 
 interface CreationItemProps {
-  image: string;
-  progect: string;
-  discr: string;
+	image: string;
+	project: string;
+	description: string;
 }
 
 const CreationItems: React.FC<{ items: CreationItemProps[] }> = ({ items }) => {
-  return (
-    <div className="creation-items">
-      {items.map((item, index) => (
-        <CreationItem
-          key={index}
-          image={item.image}
-          progect={item.progect}
-          discr={item.discr}
-        />
-      ))}
-    </div>
-  )
-}
+	return (
+		<div className='creation-items'>
+			{items.map((item, index) => (
+				<CreationItem
+					key={index}
+					image={item.image}
+					project={item.project}
+					description={item.description}
+				/>
+			))}
+		</div>
+	);
+};
 
-export default CreationItems
+export default CreationItems;
