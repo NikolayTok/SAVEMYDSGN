54113fee7a7af1dad917fab5120ecd44f7b990d7
tokkaya79 <tokkaya79@gmail.com>
2024-05-17 20:28:44 +0200

    fixed bugs

--- src/components/Stages.tsx
+++ src/components/Stages.tsx
@@ -11,10 +11,10 @@ const Stages = () => {
             You ideas<span>
               <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g filter="url(#filter0_b_1_265)">
-                  <circle cx="32" cy="32" r="32" fill="white" fill-opacity="0.1" />
-                  <circle cx="32" cy="32" r="31.5" stroke="white" stroke-opacity="0.12" />
+                  <circle cx="32" cy="32" r="32" fill="white" fillOpacity="0.1" />
+                  <circle cx="32" cy="32" r="31.5" stroke="white" strokeOpacity="0.12" />
                 </g>
-                <g clip-path="url(#clip0_1_265)">
+                <g clipPath="url(#clip0_1_265)">
                   <path d="M34.5 44H28.5C27.948 44 27.5 43.552 27.5 43C27.5 42.448 27.948 42 28.5 42H34.5C35.052 42 35.5 42.448 35.5 43C35.5 43.552 35.052 44 34.5 44Z" fill="white" />
                   <path d="M31.5 20.6093C30.948 20.6093 30.5 20.1613 30.5 19.6093V17C30.5 16.448 30.948 16 31.5 16C32.052 16 32.5 16.448 32.5 17V19.6093C32.5 20.1613 32.052 20.6093 31.5 20.6093Z" fill="white" />
                   <path d="M40.2613 24.2387C40.0053 24.2387 39.7493 24.1413 39.5547 23.9453C39.164 23.5547 39.164 22.9213 39.5547 22.5307L41.4 20.6853C41.7907 20.2947 42.424 20.2947 42.8147 20.6853C43.2053 21.076 43.2053 21.7093 42.8147 22.1L40.9693 23.9453C40.7733 24.14 40.5173 24.2387 40.2613 24.2387Z" fill="white" />
@@ -27,8 +27,8 @@ const Stages = () => {
                   <path d="M33.1671 48H29.8337C28.7097 48 27.5004 47.1466 27.5004 45.2746V42.9226C27.5004 41.508 26.8977 40.2093 25.8884 39.4493C22.9751 37.256 21.6271 33.648 22.3697 30.0333C23.0964 26.492 25.9617 23.616 29.4991 22.876C32.3111 22.2853 35.1897 22.9693 37.3844 24.7533C39.5764 26.5346 40.8337 29.176 40.8337 32C40.8337 34.8773 39.5337 37.548 37.2671 39.3306C36.1444 40.2133 35.5004 41.4426 35.5004 42.7026V45.6666C35.5004 46.9533 34.4537 48 33.1671 48ZM31.4817 24.6693C30.9591 24.6693 30.4324 24.724 29.9057 24.8333C27.1377 25.412 24.8964 27.6626 24.3271 30.4346C23.7417 33.284 24.8004 36.1266 27.0897 37.8506C28.5977 38.9866 29.4991 40.8826 29.4991 42.9226V45.2746C29.4991 45.396 29.5151 46 29.8324 46H33.1657C33.3497 46 33.4991 45.8506 33.4991 45.6666V42.7026C33.4991 40.824 34.4217 39.0226 36.0297 37.7573C37.8124 36.3573 38.8337 34.2586 38.8337 32C38.8337 29.7813 37.8444 27.7053 36.1217 26.3053C34.8044 25.236 33.1711 24.6693 31.4817 24.6693Z" fill="white" />
                 </g>
                 <defs>
-                  <filter id="filter0_b_1_265" x="-60" y="-60" width="184" height="184" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                  <filter id="filter0_b_1_265" x="-60" y="-60" width="184" height="184" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
                     <feGaussianBlur in="BackgroundImageFix" stdDeviation="30" />
                     <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_1_265" />
                     <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_1_265" result="shape" />
@@ -40,11 +40,11 @@ const Stages = () => {
               </svg>
             </span>our designs<span>
               <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="32" cy="32" r="32" fill="white" fill-opacity="0.1" />
-                <circle cx="32" cy="32" r="31.5" stroke="white" stroke-opacity="0.12" />
-                <path d="M24.0513 22.5483C24.4413 22.1026 25.0047 21.8469 25.597 21.8469H37.212C37.8042 21.8469 38.3677 22.1026 38.7577 22.5483L43.6611 28.1522C44.3552 28.9455 44.3363 30.1356 43.6168 30.9062L31.4045 43.9908L19.1921 30.9062C18.4728 30.1356 18.4537 28.9455 19.1479 28.1522L24.0513 22.5483Z" stroke="white" stroke-width="1.54045" stroke-linecap="round" stroke-linejoin="round" />
-                <path d="M37.1813 29.5492L31.4046 43.9908L25.6278 29.5492L31.4046 21.8469L37.1813 29.5492Z" stroke="white" stroke-width="1.54045" stroke-linecap="round" stroke-linejoin="round" />
-                <path d="M19.0809 29.3567H43.7283" stroke="white" stroke-width="1.54045" stroke-linecap="round" stroke-linejoin="round" />
+                <circle cx="32" cy="32" r="32" fill="white" fillOpacity="0.1" />
+                <circle cx="32" cy="32" r="31.5" stroke="white" strokeOpacity="0.12" />
+                <path d="M24.0513 22.5483C24.4413 22.1026 25.0047 21.8469 25.597 21.8469H37.212C37.8042 21.8469 38.3677 22.1026 38.7577 22.5483L43.6611 28.1522C44.3552 28.9455 44.3363 30.1356 43.6168 30.9062L31.4045 43.9908L19.1921 30.9062C18.4728 30.1356 18.4537 28.9455 19.1479 28.1522L24.0513 22.5483Z" stroke="white" strokeWidth="1.54045" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M37.1813 29.5492L31.4046 43.9908L25.6278 29.5492L31.4046 21.8469L37.1813 29.5492Z" stroke="white" strokeWidth="1.54045" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M19.0809 29.3567H43.7283" stroke="white" strokeWidth="1.54045" strokeLinecap="round" strokeLinejoin="round" />
               </svg>
             </span>
           </h2>
