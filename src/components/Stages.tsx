e4d3c60e3d79f44c54df5989d812b247e44cda93
tokkaya79 <tokkaya79@gmail.com>
2024-05-26 20:16:27 +0200

    corrected according to the new responsive design in figma

--- src/components/Stages.tsx
+++ src/components/Stages.tsx
@@ -6,11 +6,10 @@ import "../styles/stages.scss";
 const Stages = () => {
   return (
     <section className="stages">
-      <div className="container">
         <div className="stages__wrapper">
           <div className="stages__title-box">
-            <h2 className="stages__title">
-              You ideas<span>
+            <h2 className="stages__title  title">
+              You ideas<span className="stages__title__span-idea">
                 <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <g filter="url(#filter0_b_1_265)">
                     <circle cx="32" cy="32" r="32" fill="white" fillOpacity="0.1" />
@@ -40,7 +39,7 @@ const Stages = () => {
                     </clipPath>
                   </defs>
                 </svg>
-              </span>our designs<span>
+              </span> our designs<span className="stages__title__span-design">
                 <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <circle cx="32" cy="32" r="32" fill="white" fillOpacity="0.1" />
                   <circle cx="32" cy="32" r="31.5" stroke="white" strokeOpacity="0.12" />
@@ -51,7 +50,7 @@ const Stages = () => {
               </span>
             </h2>
           </div>
-          <p className="stages__sub-title">
+          <p className="stages__sub-title subtitle">
             Navigating the creative stages
           </p>
           <div className="stages__list">
@@ -75,7 +74,6 @@ const Stages = () => {
             />
           </div>
         </div>
-      </div> 
     </section>
   );
 };
