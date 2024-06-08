c1e88fba9074f6334a15d34eedac797e121149ac
tokkaya79 <tokkaya79@gmail.com>
2024-06-08 19:46:39 +0200

    update section BookDemo

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,28 +1,32 @@
-import React from 'react';
-import Sturzl from '@/components/Sturzl';
-import Project from '@/components/Project';
-import TwoBlocks from '@/components/TwoBlocks';
-import SturzlSlider from '@/components/SturzlSlider';
-import TwoSquares from '@/components/TwoSquares';
-import BottomBlock from '@/components/BottomBlock';
-import BookDemo from '@/components/BookDemo';
+import React from "react";
+import Sturzl from "@/components/Sturzl";
+import Project from "@/components/Project";
+import TwoBlocks from "@/components/TwoBlocks";
+import SturzlSlider from "@/components/SturzlSlider";
+import TwoSquares from "@/components/TwoSquares";
+import BottomBlock from "@/components/BottomBlock";
+import BookDemo from "@/components/BookDemo";
 
-import '../styles/home.scss';
+import "../styles/home.scss";
 
 const About = () => {
-  return (
-    <section className='hero'>
-      <h1 className='hero__title'>About</h1>
-      <span className='hero__sub-title'>Your monthly pass to design joy</span>
-      <Sturzl/>
-      <Project/>
-      <TwoBlocks/>
-      <SturzlSlider/>
-      <TwoSquares/>
-      <BottomBlock/>
-      <BookDemo/>
-    </section>
-  );
+	return (
+		<section className='hero'>
+			<h1 className='hero__title'>About</h1>
+			<span className='hero__sub-title'>Your monthly pass to design joy</span>
+			<Sturzl />
+			<Project />
+			<TwoBlocks />
+			<SturzlSlider />
+			<TwoSquares />
+			<BottomBlock />
+			<BookDemo
+				className='sturzl-book-demo'
+				buttonClassName='sturzl-book-demo__btn'
+				textClassName='sturzl-book-demo__text'
+			/>
+		</section>
+	);
 };
 
 export default About;
