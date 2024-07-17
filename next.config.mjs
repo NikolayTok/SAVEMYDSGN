9e9d4404d9f01f66e0e83c37537f47390658b3be
Mykola Tokarskyi <mykola.tokarskyi@trisbee.com>
2024-07-17 18:17:51 +0200

    config v3

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-export default {
-  output: 'export',
-  basePath: '/SAVE-MY-DESIGN', // замените на имя вашего репозитория
-  assetPrefix: '/SAVE-MY-DESIGN/', // замените на имя вашего репозитория
-  images: {
-    unoptimized: true,
-  },
+const nextConfig = {
+  output:'export',
 };
+/* module.exports = nextConfig*/
+export default nextConfig;
